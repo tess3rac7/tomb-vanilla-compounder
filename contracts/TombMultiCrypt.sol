8610e298f3d3fd6daf3fb1b6262b29712ac9a445
tess3rac7 <tess3rac7@protonmail.com>
2021-12-10 23:41:11 -0500

    Add _settleAccount internal functions
    Leaving them as separate functions for _settleLPAccount and _settleTSHAREAccount for now to make it simple to understand. Can be refactored to call through to a single function with different params later.

--- contracts/TombMultiCrypt.sol
+++ contracts/TombMultiCrypt.sol
@@ -17,6 +17,11 @@ contract TombMultiCrypt is TombVanillaCompounder {
     mapping(uint256 => uint256) public TSHAREEarnedPerLPUptoWindow;
     mapping(uint256 => uint256) public LPEarnedPerTSHAREUptoWindow;
 
+    /*
+    * the "phaseIndex" for each item in the array will be unique.
+    * this is because withdraw/deposit operations within the same phase
+    * operate on the same Deposit object
+    */
     mapping(address => Deposit[]) public LPDepositsForUser;
     mapping(address => Deposit[]) public TSHAREDepositsForUser;
 
@@ -130,11 +135,11 @@ contract TombMultiCrypt is TombVanillaCompounder {
     */
 
     function depositFTM() external payable {
-        // convert to LP then register into unlocked mapping
+        // convert to LP then call depositLP
     }
 
     function depositTOMB(uint256 _amount) external {
-        // convert to LP then register into unlocked mapping
+        // convert to LP then call depositLP
     }
 
     function depositLP(uint256 _amount) external {
@@ -144,15 +149,17 @@ contract TombMultiCrypt is TombVanillaCompounder {
     }
 
     function depositTSHARE(uint256 _amount) external {
-        // register into unlocked mapping
+        // require _amount > 0
+        // push new element
+        // call settleAccount
     }
 
     /*
     * USER WITHDRAW FUNCTIONS
-    * only unlocked amounts can be withdrawn
     */
 
     function withdrawLP(uint256 _amount) external {
+        // require withdrawableBalance >= _amount
         // call settleAccount
         // require lastElement's amount >= _amount
         // subtract and transfer
@@ -160,8 +167,11 @@ contract TombMultiCrypt is TombVanillaCompounder {
     }
 
     function withdrawTSHARE() external {
-        // pay back and deregister from unlocked mapping
-        // must have enough unlocked balance
+        // require withdrawableBalance >= _amount
+        // call settleAccount
+        // require lastElement's amount >= _amount
+        // subtract and transfer
+        // if last element's balance becomes 0, pop
     }
 
     /*
@@ -169,7 +179,7 @@ contract TombMultiCrypt is TombVanillaCompounder {
     */
 
     function lock() external onlyRole(OPERATOR_ROLE) {
-        // move all unlocked LP mappings to locked LP mappings
+
     }
 
     function unlock() external onlyRole(OPERATOR_ROLE) {
@@ -180,22 +190,89 @@ contract TombMultiCrypt is TombVanillaCompounder {
     * INTERNAL FUNCTIONS
     */
 
-    // only to be called when withdrawing or depositing
+    /*
+    * call right after depositing, or right before withdrawing
+    *
+    * _settleAccount function trims the deposits array to only 1 or 2 items.
+    * it essentially moves all unrealized gains into a user's balance
+    * and updates the phaseIndex to be current
+    */
     function _settleLPAccount(address _user) internal {
-        // for open window, we are good if:
-        //  1. array is size 1 with currentPhaseIndex
+        Deposit[] storage deposits = LPDepositsForUser[_user];
+        if (deposits.length == 0) return; // early return for empty array
 
-        // for closed window, we are good if
-        //  1. array is size 1 with currentPhaseIndex OR
-        //  2. array is size 1 with currentPhaseIndex - 1 OR
-        //  3. array is size 2 with first element being currentPhaseIndex - 1 and second element being currentPhaseIndex
+        if (_isCurrentPhaseOpen()) {
+            if (deposits.length != 1 || deposits[0].phaseIndex != currentPhaseIndex) {
+                deposits[0].amount = getLPBalance(_user);
+                deposits[0].phaseIndex = currentPhaseIndex;
 
-        // if open window
-        //  if array is size 1 with currentPhaseIndex we are good, otherwise make it size 1 (with currentPhaseIndex)
-        // else (closed window)
-        //  if array is size 1 OR [if array is size 2 and last element's phaseIndex is currentPhaseIndex] we are good
-        //  otherwise make it size 1 or 2 accordingly
+                // pop until array is length 1
+                while(deposits.length > 1) deposits.pop();
+            }
+        } else { // current phase is closed, ONLY 3 cases possible
+            if (deposits.length == 1 && deposits[0].phaseIndex == currentPhaseIndex) {
+                // case 1: user only has one deposit in current phase, no-op
+            } else if (deposits.length > 1 && deposits[deposits.length - 1].phaseIndex == currentPhaseIndex) {
+                // case 2: user has multiple deposits and the last one is in current phase
+                // in this case we need to leave the last deposit alone and consolidate the ones behind it
+                deposits[0].amount = getLPBalance(_user) - deposits[deposits.length - 1].amount;
+                deposits[0].phaseIndex = currentPhaseIndex - 1; // as if the user had deposited in the previous (open) phase
 
+                // move latest deposit from current phase into index 1
+                deposits[1].amount = deposits[deposits.length - 1].amount;
+                deposits[1].phaseIndex = currentPhaseIndex;
+
+                // pop until array is length 2
+                while(deposits.length > 2) deposits.pop();
+            } else {
+                // case 3: user only has deposits (one or multiple) from before the current phase
+                // in this case we just consolidate all of them with phaseIndex as (current - 1)
+                deposits[0].amount = getLPBalance(_user);
+                deposits[0].phaseIndex = currentPhaseIndex - 1;
+
+                // pop until array is length 1
+                while(deposits.length > 1) deposits.pop();
+            }
+        }
+    }
+
+    function _settleTSHAREAccount(address _user) internal {
+        Deposit[] storage deposits = TSHAREDepositsForUser[_user];
+        if (deposits.length == 0) return; // early return for empty array
+
+        if (_isCurrentPhaseOpen()) {
+            if (deposits.length != 1 || deposits[0].phaseIndex != currentPhaseIndex) {
+                deposits[0].amount = getTSHAREBalance(_user);
+                deposits[0].phaseIndex = currentPhaseIndex;
+
+                // pop until array is length 1
+                while(deposits.length > 1) deposits.pop();
+            }
+        } else { // current phase is closed, ONLY 3 cases possible
+            if (deposits.length == 1 && deposits[0].phaseIndex == currentPhaseIndex) {
+                // case 1: user only has one deposit in current phase, no-op
+            } else if (deposits.length > 1 && deposits[deposits.length - 1].phaseIndex == currentPhaseIndex) {
+                // case 2: user has multiple deposits and the last one is in current phase
+                // in this case we need to leave the last deposit alone and consolidate the ones behind it
+                deposits[0].amount = getTSHAREBalance(_user) - deposits[deposits.length - 1].amount;
+                deposits[0].phaseIndex = currentPhaseIndex - 1; // as if the user had deposited in the previous (open) phase
+
+                // move latest deposit from current phase into index 1
+                deposits[1].amount = deposits[deposits.length - 1].amount;
+                deposits[1].phaseIndex = currentPhaseIndex;
+
+                // pop until array is length 2
+                while(deposits.length > 2) deposits.pop();
+            } else {
+                // case 3: user only has deposits (one or multiple) from before the current phase
+                // in this case we just consolidate all of them with phaseIndex as (current - 1)
+                deposits[0].amount = getTSHAREBalance(_user);
+                deposits[0].phaseIndex = currentPhaseIndex - 1;
+
+                // pop until array is length 1
+                while(deposits.length > 1) deposits.pop();
+            }
+        }
     }
 
     function _getLastClosedPhaseIndex() internal view returns (uint256) {
